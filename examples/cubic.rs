d342fba0676609cfe46fd2fc621ba40b8bd3fcda
Gnana Prakash <gnanaprakashpunnavajhala@gmail.com>
2024-05-01 19:08:08 +0530

    Got the R1CSLite instance to work MF 😤💪

--- examples/cubic.rs
+++ examples/cubic.rs
@@ -25,8 +25,8 @@ fn r1cs_lite() -> (
   InputsAssignment,
 ) {
   // parameters of the R1CS instance
-  let num_cons = 4;
-  let num_vars = 4;
+  let num_cons = 7;
+  let num_vars = 5;
   let num_inputs = 1;
   let num_non_zero_entries = 8;
 
@@ -38,41 +38,56 @@ fn r1cs_lite() -> (
 
   let one = Scalar::ONE.to_bytes();
 
-  // constraint 0 entries in (A,B,C)
-  // constraint 0 is Z0 * Z0 - Z1 = 0.
-  A.push((0, 0, one));
-  B.push((0, 0, one));
-  C.push((0, 1, one));
+  // R1CSLite instance for y = x^16 + 1
+  // Constraints are written in the order of A.B = C
+  // For now, C will be identity - Just testing out the correctness of our R1CSLite instance for y = x^16 + 1
 
-  // constraint 1 entries in (A,B,C)
-  // constraint 1 is Z1 * Z0 - Z2 = 0.
-  A.push((1, 1, one));
+  // Constraint 0 is x * 1 = x
+  A.push((0, 0,        one));
+  B.push((0, num_vars, one));
+  C.push((0, 0,        one));
+
+  // Constraint 1 is x * x = a
+  A.push((1, 0, one));
   B.push((1, 0, one));
-  C.push((1, 2, one));
+  C.push((1, 1, one));
 
-  // constraint 2 entries in (A,B,C)
-  // constraint 2 is (Z2 + Z0) * 1 - Z3 = 0.
-  A.push((2, 2, one));
-  A.push((2, 0, one));
-  B.push((2, num_vars, one));
-  C.push((2, 3, one));
+  // Constraint 2 is a * a = b
+  A.push((2, 1, one));
+  B.push((2, 1, one));
+  C.push((2, 2, one));
 
-  // constraint 3 entries in (A,B,C)
-  // constraint 3 is (Z3 + 5) * 1 - I0 = 0.
-  A.push((3, 3, one));
-  A.push((3, num_vars, Scalar::from(5u32).to_bytes()));
-  B.push((3, num_vars, one));
-  C.push((3, num_vars + 1, one));
+  // Constraint 3 is b * b = c
+  A.push((3, 2, one));
+  B.push((3, 2, one));
+  C.push((3, 3, one));
+
+  // Constraint 4 is c * c = d
+  A.push((4, 3, one));
+  B.push((4, 3, one));
+  C.push((4, 4, one));
+
+  // Constraint 5 is 1 * 1 = 1
+  A.push((5, num_vars, one));
+  B.push((5, num_vars, one));
+  C.push((5, num_vars, one));
+
+  // Constraint 6 is (d + 1) * 1 = y
+  A.push((6, 4, one));
+  A.push((6, num_vars,     one));
+  B.push((6, num_vars,     one));
+  C.push((6, num_vars + 1, one));
 
   let inst = Instance::new(num_cons, num_vars, num_inputs, &A, &B, &C).unwrap();
 
   // compute a satisfying assignment
   let mut csprng: OsRng = OsRng;
   let z0 = Scalar::random(&mut csprng);
-  let z1 = z0 * z0; // constraint 0
-  let z2 = z1 * z0; // constraint 1
-  let z3 = z2 + z0; // constraint 2
-  let i0 = z3 + Scalar::from(5u32); // constraint 3
+  let z1 = z0 * z0;          // constraint 2
+  let z2 = z1 * z1;          // constraint 3
+  let z3 = z2 * z2;          // constraint 4
+  let z4 = z3 * z3;          // constraint 5
+  let i0 = z4 + Scalar::ONE; // constraint 6
 
   // create a VarsAssignment
   let mut vars = vec![Scalar::ZERO.to_bytes(); num_vars];
@@ -80,6 +95,7 @@ fn r1cs_lite() -> (
   vars[1] = z1.to_bytes();
   vars[2] = z2.to_bytes();
   vars[3] = z3.to_bytes();
+  vars[4] = z4.to_bytes();
   let assignment_vars = VarsAssignment::new(&vars).unwrap();
 
   // create an InputsAssignment
