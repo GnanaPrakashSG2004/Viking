689ef286601859f343d2fa3014197980b749281e
Gnana Prakash <gnanaprakashpunnavajhala@gmail.com>
2024-05-01 22:55:49 +0530

    R1CS vs Lite metrics on same example

--- examples/cubic.rs
+++ examples/cubic.rs
@@ -118,6 +118,99 @@ fn r1cs_lite() -> (
   )
 }
 
+fn r1cs() -> (
+  usize,
+  usize,
+  usize,
+  usize,
+  Instance,
+  VarsAssignment,
+  InputsAssignment,
+) {
+  // parameters of the R1CS instance
+  let num_cons = 5;
+  let num_vars = 5;
+  let num_inputs = 1;
+  let num_non_zero_entries = 6;
+
+  // We will encode the above constraints into three matrices, where
+  // the coefficients in the matrix are in the little-endian byte order
+  let mut A: Vec<(usize, usize, [u8; 32])> = Vec::new();
+  let mut B: Vec<(usize, usize, [u8; 32])> = Vec::new();
+  let mut C: Vec<(usize, usize, [u8; 32])> = Vec::new();
+
+  let one = Scalar::ONE.to_bytes();
+
+  // R1CSLite instance for y = x^16 + 1
+  // Constraints are written in the order of A.B = C
+  // For now, C will be identity - Just testing out the correctness of our R1CSLite instance for y = x^16 + 1
+
+  // Constraint 0 is x * x = a
+  A.push((0, 0, one));
+  B.push((0, 0, one));
+  C.push((0, 1, one));
+
+  // Constraint 1 is a * a = b
+  A.push((1, 1, one));
+  B.push((1, 1, one));
+  C.push((1, 2, one));
+
+  // Constraint 2 is b * b = c
+  A.push((2, 2, one));
+  B.push((2, 2, one));
+  C.push((2, 3, one));
+
+  // Constraint 3 is c * c = d
+  A.push((3, 3, one));
+  B.push((3, 3, one));
+  C.push((3, 4, one));
+
+  // Constraint 4 is (d + 1) * 1 = y
+  A.push((4, 4, one));
+  A.push((4, num_vars,     one));
+  B.push((4, num_vars,     one));
+  C.push((4, num_vars + 1, one));
+
+  let inst = Instance::new(num_cons, num_vars, num_inputs, &A, &B, &C).unwrap();
+
+  // compute a satisfying assignment
+  let mut csprng: OsRng = OsRng;
+  let z0 = Scalar::random(&mut csprng);
+  let z1 = z0 * z0;          // constraint 2
+  let z2 = z1 * z1;          // constraint 3
+  let z3 = z2 * z2;          // constraint 4
+  let z4 = z3 * z3;          // constraint 5
+  let i0 = z4 + Scalar::ONE; // constraint 6
+
+  // create a VarsAssignment
+  let mut vars = vec![Scalar::ZERO.to_bytes(); num_vars];
+  vars[0] = z0.to_bytes();
+  vars[1] = z1.to_bytes();
+  vars[2] = z2.to_bytes();
+  vars[3] = z3.to_bytes();
+  vars[4] = z4.to_bytes();
+  let assignment_vars = VarsAssignment::new(&vars).unwrap();
+
+  // create an InputsAssignment
+  let mut inputs = vec![Scalar::ZERO.to_bytes(); num_inputs];
+  inputs[0] = i0.to_bytes();
+  let assignment_inputs = InputsAssignment::new(&inputs).unwrap();
+
+  // check if the instance we created is satisfiable
+  let res = inst.is_sat(&assignment_vars, &assignment_inputs);
+  assert!(res.unwrap(), "should be satisfied");
+
+  (
+    num_cons,
+    num_vars,
+    num_inputs,
+    num_non_zero_entries,
+    inst,
+    assignment_vars,
+    assignment_inputs,
+  )
+}
+
 fn main() {
   // produce an R1CS instance
   let (
