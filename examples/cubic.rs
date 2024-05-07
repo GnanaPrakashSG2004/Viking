4df19e98f95e53e3ba978dd5080255e7600da07b
Shrikara A <shrikara.a@montycloud.com>
2024-05-07 16:30:47 +0530

    fix: Correct R1CS example

--- examples/cubic.rs
+++ examples/cubic.rs
@@ -2,20 +2,23 @@
 //! The example is described in detail [here].
 //!
 //! The R1CS for this problem consists of the following 4 constraints:
-//! `Z0 * Z0 - Z1 = 0`
-//! `Z1 * Z0 - Z2 = 0`
-//! `(Z2 + Z0) * 1 - Z3 = 0`
-//! `(Z3 + 5) * 1 - I0 = 0`
+//! x * 1 = x
+//! x * x = a
+//! a * a = b
+//! b * b = c
+//! c * c = d
+//! e * e = e
+//! (d + 1) * 1 = y
 //!
 //! [here]: https://medium.com/@VitalikButerin/quadratic-arithmetic-programs-from-zero-to-hero-f6d558cea649
+#![allow(non_snake_case)]
 #![allow(clippy::assertions_on_result_states)]
 use curve25519_dalek::scalar::Scalar;
 use libspartan::{InputsAssignment, Instance, SNARKGens, VarsAssignment, SNARK};
 use merlin::Transcript;
 use rand::rngs::OsRng;
 
-#[allow(non_snake_case)]
-fn produce_r1cs() -> (
+fn r1cs() -> (
   usize,
   usize,
   usize,
@@ -25,10 +28,10 @@ fn produce_r1cs() -> (
   InputsAssignment,
 ) {
   // parameters of the R1CS instance
-  let num_cons = 4;
-  let num_vars = 4;
+  let num_cons = 5;
+  let num_vars = 5;
   let num_inputs = 1;
-  let num_non_zero_entries = 8;
+  let num_non_zero_entries = 6;
 
   // We will encode the above constraints into three matrices, where
   // the coefficients in the matrix are in the little-endian byte order
@@ -38,46 +41,46 @@ fn produce_r1cs() -> (
 
   let one = Scalar::ONE.to_bytes();
 
-  // R1CS is a set of three sparse matrices A B C, where is a row for every
-  // constraint and a column for every entry in z = (vars, 1, inputs)
-  // An R1CS instance is satisfiable iff:
-  // Az \circ Bz = Cz, where z = (vars, 1, inputs)
+  // R1CSLite instance for y = x^16 + 1
+  // Constraints are written in the order of A.B = C
+  // For now, C will be identity - Just testing out the correctness of our R1CSLite instance for y = x^16 + 1
 
-  // constraint 0 entries in (A,B,C)
-  // constraint 0 is Z0 * Z0 - Z1 = 0.
+  // Constraint 0 is x * x = a
   A.push((0, 0, one));
   B.push((0, 0, one));
   C.push((0, 1, one));
 
-  // constraint 1 entries in (A,B,C)
-  // constraint 1 is Z1 * Z0 - Z2 = 0.
+  // Constraint 1 is a * a = b
   A.push((1, 1, one));
-  B.push((1, 0, one));
+  B.push((1, 1, one));
   C.push((1, 2, one));
 
-  // constraint 2 entries in (A,B,C)
-  // constraint 2 is (Z2 + Z0) * 1 - Z3 = 0.
+  // Constraint 2 is b * b = c
   A.push((2, 2, one));
-  A.push((2, 0, one));
-  B.push((2, num_vars, one));
+  B.push((2, 2, one));
   C.push((2, 3, one));
 
-  // constraint 3 entries in (A,B,C)
-  // constraint 3 is (Z3 + 5) * 1 - I0 = 0.
+  // Constraint 3 is c * c = d
   A.push((3, 3, one));
-  A.push((3, num_vars, Scalar::from(5u32).to_bytes()));
-  B.push((3, num_vars, one));
-  C.push((3, num_vars + 1, one));
+  B.push((3, 3, one));
+  C.push((3, 4, one));
+
+  // Constraint 4 is (d + 1) * 1 = y
+  A.push((4, 4, one));
+  A.push((4, num_vars,     one));
+  B.push((4, num_vars,     one));
+  C.push((4, num_vars + 1, one));
 
   let inst = Instance::new(num_cons, num_vars, num_inputs, &A, &B, &C).unwrap();
 
   // compute a satisfying assignment
   let mut csprng: OsRng = OsRng;
   let z0 = Scalar::random(&mut csprng);
-  let z1 = z0 * z0; // constraint 0
-  let z2 = z1 * z0; // constraint 1
-  let z3 = z2 + z0; // constraint 2
-  let i0 = z3 + Scalar::from(5u32); // constraint 3
+  let z1 = z0 * z0;          // constraint 2
+  let z2 = z1 * z1;          // constraint 3
+  let z3 = z2 * z2;          // constraint 4
+  let z4 = z3 * z3;          // constraint 5
+  let i0 = z4 + Scalar::ONE; // constraint 6
 
   // create a VarsAssignment
   let mut vars = vec![Scalar::ZERO.to_bytes(); num_vars];
@@ -85,6 +88,7 @@ fn produce_r1cs() -> (
   vars[1] = z1.to_bytes();
   vars[2] = z2.to_bytes();
   vars[3] = z3.to_bytes();
+  vars[4] = z4.to_bytes();
   let assignment_vars = VarsAssignment::new(&vars).unwrap();
 
   // create an InputsAssignment
@@ -107,82 +111,6 @@ fn produce_r1cs() -> (
   )
 }
 
-
-fn r1cs_lite() -> (usize, usize, usize, usize, Instance, VarsAssignment, InputsAssignment) {
-  // parameters of the R1CS instance
-  let num_cons = 4;
-  let num_vars = 4;
-  let num_inputs = 1;
-  let num_non_zero_entries = 8;
-
-  // We will encode the above constraints into three matrices, where
-  // the coefficients in the matrix are in the little-endian byte order
-  let mut A: Vec<(usize, usize, [u8; 32])> = Vec::new();
-  let mut B: Vec<(usize, usize, [u8; 32])> = Vec::new();
-  let mut C: Vec<(usize, usize, [u8; 32])> = Vec::new();
-
-  let one = Scalar::ONE.to_bytes();
-
-  // constraint 0 entries in (A,B,C)
-  // constraint 0 is Z0 * Z0 - Z1 = 0.
-  A.push((0, 0, one));
-  B.push((0, 0, one));
-  C.push((0, 1, one));
-
-  // constraint 1 entries in (A,B,C)
-  // constraint 1 is Z1 * Z0 - Z2 = 0.
-  A.push((1, 1, one));
-  B.push((1, 0, one));
-  C.push((1, 2, one));
-
-  // constraint 2 entries in (A,B,C)
-  // constraint 2 is (Z2 + Z0) * 1 - Z3 = 0.
-  A.push((2, 2, one));
-  A.push((2, 0, one));
-  B.push((2, num_vars, one));
-  C.push((2, 3, one));
-
-  // constraint 3 entries in (A,B,C)
-  // constraint 3 is (Z3 + 5) * 1 - I0 = 0.
-  A.push((3, 3, one));
-  A.push((3, num_vars, Scalar::from(5u32).to_bytes()));
-  B.push((3, num_vars, one));
-  C.push((3, num_vars + 1, one));
-
-  let inst = Instance::new(num_cons, num_vars, num_inputs, &A, &B, &C).unwrap();
-
-  // compute a satisfying assignment
-  let mut csprng: OsRng = OsRng;
-  let z0 = Scalar::random(&mut csprng);
-  let z1 = z0 * z0; // constraint 0
-  let z2 = z1 * z0; // constraint 1
-  let z3 = z2 + z0; // constraint 2
-  let i0 = z3 + Scalar::from(5u32); // constraint 3
-
-  // create a VarsAssignment
-  let mut vars = vec![Scalar::ZERO.to_bytes(); num_vars];
-  vars[0] = z0.to_bytes();
-  vars[1] = z1.to_bytes();
-  vars[2] = z2.to_bytes();
-  vars[3] = z3.to_bytes();
-  let assignment_vars = VarsAssignment::new(&vars).unwrap();
-
-  // create an InputsAssignment
-  let mut inputs = vec![Scalar::ZERO.to_bytes(); num_inputs];
-  inputs[0] = i0.to_bytes();
-  let assignment_inputs = InputsAssignment::new(&inputs).unwrap();
-
-  (
-    num_cons,
-    num_vars,
-    num_inputs,
-    num_non_zero_entries,
-    inst,
-    assignment_vars,
-    assignment_inputs,
-  )
-}
-
 fn main() {
   // produce an R1CS instance
   let (
@@ -193,7 +121,7 @@ fn main() {
     inst,
     assignment_vars,
     assignment_inputs,
-  ) = produce_r1cs();
+  ) = r1cs();
 
   // produce public parameters
   let gens = SNARKGens::new(num_cons, num_vars, num_inputs, num_non_zero_entries);
